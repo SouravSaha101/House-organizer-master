32c32ab5a1c8f616d0a89f064612cddda21cf1ae
Sourav <souravsaha101@gmail.com>
2021-04-13 00:50:28 +0530

    Nav Bar Added

--- house-organizer/src/App.js
+++ house-organizer/src/App.js
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <div>
-        <h1>Hello World</h1>
+      <div id="nav">
+        <nav className="navbar navbar-dark  bg-dark fixed-top" id="mainNav">
+          <a className="navbar-brand  mr-0">Home Organizer</a>
+          <div className="float-right buttons">
+            <a className="btn btn-lg btn-link text-primary">Log in</a>
+            <a className="btn btn-lg bg-primary text-white font-weight-bold">
+              Sign up
+            </a>
+          </div>
+        </nav>
       </div>
     </div>
   );
