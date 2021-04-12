43934a39f7ded97b413d6861b8d7465a155c03b6
Sourav <souravsaha101@gmail.com>
2021-04-13 02:52:39 +0530

    footer added

--- house-organizer/src/App.js
+++ house-organizer/src/App.js
@@ -59,6 +59,29 @@ const App = () => {
           </div>
         </div>
       </section>
+
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <span className="copyright">
+                &copy; Copyright 2021. All rights reserved
+              </span>
+            </div>
+            <div className="col-md-4"></div>
+            <div className="col-md-4">
+              <ul className="list-inline quicklinks">
+                <li className="list-inline-item">
+                  <a href="#">Privacy Policy</a>
+                </li>
+                <li className="list-inline-item">
+                  <a href="#">Terms of Use</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
