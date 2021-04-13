943d7911242ff12b683bd1ed9dbe9452fca14104
Sourav <souravsaha101@gmail.com>
2021-04-13 14:55:26 +0530

    Changes

--- house-organizer/src/App.js
+++ house-organizer/src/App.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Footer from "./Components/Footer/Footer";
 import HomeNav from "./Components/Home-Nav/HomeNav";
+
 const App = () => {
+  const [scroll, setScroll] = useState(0);
+
+  useEffect(() => {
+    document.addEventListener("scroll", () => {
+      const scrollCheck = window.scrollY < 100;
+      if (scrollCheck !== scroll) {
+        setScroll(scrollCheck);
+      }
+    });
+  });
+
   return (
     <div className="App">
       <HomeNav />
       <section id="home">
-        <div className="container pt-10 pb-5 text-center">
+        <div className="container pt-10 pb-5 text-md-left">
           <div className="row align-items-center">
-            <div className="col-lg-7">
-              <div className="intro-heading text-uppercase">
-                Home Organizing Lavra Lahsun
+            <div className="col-lg-9">
+              <div className="intro-heading">
+                Organizing have never been more fun
               </div>
-              <h3 className="mt-5">
+              <p className="mt-5">
                 Dil kyun yeh mera shor kare Dil kyun yeh mera shor kare Idhar
                 nahi Udhar nahi Teri Ore Chale.
-              </h3>
+              </p>
               <form className="row my-5">
                 <div className="d-none d-md-block col-md-7 pr-md-2">
                   <input
@@ -29,14 +41,14 @@ const App = () => {
                 <div className="col-md-5 pl-md-2">
                   <button
                     type="submit"
-                    className="btn btn-wrap btn-primary btn-block px-4"
+                    className="btn btn-wrap btn-primary btn-block btn-lg"
                   >
                     Sign up—it’s free!
                   </button>
                 </div>
               </form>
             </div>
-            <div className="col-lg-5 home-image">
+            <div className="col-lg-3 home-image">
               <img
                 src="'./Assests/Style/computer-notebook.jpg'"
                 width="931"
